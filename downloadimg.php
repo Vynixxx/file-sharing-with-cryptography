2df04b178d43ab57cf1b27dcef94c63c20a03bc2
Thoriq Nadaghoist Suharyadi <109634983+Vynixxx@users.noreply.github.com>
2023-06-11 18:48:43 +0700

    Update downloadimg.php

--- downloadimg.php
+++ downloadimg.php
@@ -1,56 +1,121 @@
 <?php
-// Koneksi ke database
-$databaseHost = 'localhost'; 
-$databaseName = 'db_filesharing';
-$databaseUser = 'root'; 
-$databasePassword = ''; 
-
-$database = new mysqli($databaseHost, $databaseUser, $databasePassword, $databaseName);
-if ($database->connect_error) {
-    die("Koneksi ke database gagal: " . $database->connect_error);
+// Step 1: Koneksi ke database
+$connection = mysqli_connect("localhost", "root", "", "db_filesharing");
+if (!$connection) {
+  die("Koneksi database gagal: " . mysqli_connect_error());
 }
 
-// Mendapatkan ID file dari parameter URL
+// Step 2: Memeriksa apakah ID file diberikan
 if (isset($_GET['id'])) {
     $fileId = $_GET['id'];
 
-    // Query database untuk mendapatkan informasi file
-    $query = "SELECT * FROM files WHERE id = '$fileId'";
-    $result = $database->query($query);
+    // Step 3: Mengambil data file dari database berdasarkan ID
+    $query = "SELECT filename, content FROM files WHERE id = '$fileId'";
+    $result = mysqli_query($connection, $query);
 
-    if ($result && $result->num_rows > 0) {
-        $fileData = $result->fetch_assoc();
+    if ($result && mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        $filename = $row['filename'];
+        $encryptedContent = $row['content'];
 
-        $filename = $fileData['filename'];
-        $encryptedContent = $fileData['content'];
+        // Step 4: Meminta pengguna memasukkan kunci
+        if (isset($_POST['encryption-key'])) {
+            $encryptionKey = $_POST['encryption-key'];
 
-        // Mendekripsi konten file
-        $decryptionKey = '@AndaKepoSekaliTampaknya88218'; // kunci enkripsi
-        $encryptedText = base64_decode($encryptedContent);
-        $decryptedText = aesDecrypt($encryptedText, $decryptionKey); // Menggunakan metode AES
+            // Step 5: Mendekripsi konten file
+            $decryptedContent = aesDecrypt(base64_decode($encryptedContent), $encryptionKey);
 
-        // Mengirimkan gambar ke pengguna
-        header("Content-Type: image/jpeg"); // Sesuaikan dengan tipe file yang diunduh (misalnya: image/png untuk PNG)
-        header("Content-Disposition: attachment; filename=\"$filename\"");
-        echo $decryptedText;
-        exit();
+            // Step 6: Mengirim file ke pengguna
+            header("Content-Type: application/octet-stream");
+            header("Content-Disposition: attachment; filename=\"$filename\"");
+            echo $decryptedContent;
+            exit();
+        }
+?>
+
+<style>
+body {
+  font-family: Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+  display: flex;   
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+header {
+  background-color: #333;
+  color: #fff;
+  padding: 20px;
+}
+
+nav ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+
+nav ul li {
+  display: inline-block;
+}
+
+nav ul li a {
+  color: #fff;
+  text-decoration: none;
+  padding: 10px;
+}
+
+.container {
+  width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+  flex: 1;
+}
+
+footer {
+  background-color: #333;
+  color: #fff;
+  padding: 20px;
+  text-align: center;
+}
+
+</style>
+
+<body>
+  <header>
+    <h1>Sistem File Sharing</h1>
+  </header>
+  
+  <div class="container">
+    <h2>Download File</h2>
+
+    <form method="post" action="">
+        <label for="encryption-key">Masukkan kunci:</label>
+        <input type="password" name="encryption-key" required>
+        <input type="submit" value="Download" id="download-btn">
+    </form>
+  </div>
+
+  <footer>
+    &copy; 2023 Sistem File Sharing. All rights reserved.
+  </footer>
+</body>
+
+<?php
     } else {
         echo "File tidak ditemukan.";
     }
-} else {
-    echo "ID file tidak diberikan.";
 }
 
-// Fungsi untuk mendekripsi file menggunakan metode kriptografi AES (Advanced Encryption Standard)
+// Step 7: Menutup koneksi ke database
+mysqli_close($connection);
+
+// Fungsi untuk mendekripsi konten file menggunakan metode kriptografi AES (Advanced Encryption Standard)
 function aesDecrypt($ciphertext, $key) {
-    $ciphertext = base64_decode($ciphertext);
+    $ciphertextWithIV = base64_decode($ciphertext);
     $ivSize = openssl_cipher_iv_length('aes-256-cbc');
-    $iv = substr($ciphertext, 0, $ivSize);
-    $ciphertext = substr($ciphertext, $ivSize);
-    $plaintext = openssl_decrypt($ciphertext, 'aes-256-cbc', $key, OPENSSL_RAW_DATA, $iv);
-    return $plaintext;
+    $iv = substr($ciphertextWithIV, 0, $ivSize);
+    $encryptedText = substr($ciphertextWithIV, $ivSize);
+    return openssl_decrypt($encryptedText, 'aes-256-cbc', $key, OPENSSL_RAW_DATA, $iv);
 }
-
-// Tutup koneksi database
-$database->close();
 ?>
